83475e24b0607985a8fccf3f32f6919728203593
baolijn <libao_fighting@outlook.com>
2018-01-13 15:33:34 +0800

    add RequestParam

--- src/main/java/com/libao/HelloController.java
+++ src/main/java/com/libao/HelloController.java
@@ -2,19 +2,16 @@ package com.libao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/hello")
 public class HelloController {
    @Autowired
     private GirlProperties girlProperties;
-    @RequestMapping(value =  "/say/{id}", method = RequestMethod.GET)
-    public String say(@PathVariable("id") Integer id){
-        return "id: " + id;
+    @RequestMapping(value =  "/say", method = RequestMethod.GET)
+    public String say(@RequestParam("id") Integer myid){
+        return "id: " + myid;
         //return girlProperties.getCupSize();
     }
 }
