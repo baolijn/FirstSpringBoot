2b03acf3d4ab7b2b2c74d7d0563a6a6dde3d5cc8
baolijn <libao_fighting@outlook.com>
2018-01-13 15:30:08 +0800

    add PathVariables

--- src/main/java/com/libao/HelloController.java
+++ src/main/java/com/libao/HelloController.java
@@ -2,6 +2,7 @@ package com.libao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
    @Autowired
     private GirlProperties girlProperties;
-    @RequestMapping(value =  "/say", method = RequestMethod.GET)
-    public String say(){
-        return girlProperties.getCupSize();
+    @RequestMapping(value =  "/say/{id}", method = RequestMethod.GET)
+    public String say(@PathVariable("id") Integer id){
+        return "id: " + id;
+        //return girlProperties.getCupSize();
     }
 }
